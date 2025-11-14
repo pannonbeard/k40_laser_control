760ac51a02f7e5a413a9a7b0b98c369e16d96dad
Phil Hendricks <pfhendri1@gmail.com>
2025-11-13 21:02:17 -0600

    clean up and added async await

--- public/js/app.js
+++ public/js/app.js
@@ -1,37 +1,37 @@
-function jog(direction) {
-    fetch('/jog', {
+async function jog(direction) {
+  await fetch('/jog', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({direction: direction, distance: 10})
+  });
+}
+
+async function testFire() {
+  if(confirm('Fire laser for 0.5s?')) {
+    await fetch('/laser/fire', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({direction: direction, distance: 10})
+      body: JSON.stringify({power: 10, duration: 0.5})
     });
   }
+}
 
-  function testFire() {
-    if(confirm('Fire laser for 0.5s?')) {
-      fetch('/laser/fire', {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({power: 10, duration: 0.5})
-      });
-    }
+function refreshPage() {
+  window.location.reload()
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+  let laserConnecting = document.querySelector('.laser-connecting')
+
+  laserConnecting.style.display = 'block'
+
+  let response = await fetch('/connect', { headers: {}, method: 'POST', body: JSON.stringify({}) })
+
+  let data = await response.json()
+
+  if(data.message){
+    laserConnecting.innerText = data.message
+  } else {
+    laserConnecting.innerText = 'Laser Connected'
   }
-
-  function refreshPage() {
-    window.location.reload()
-  }
-
-  document.addEventListener('DOMContentLoaded', async () => {
-    let laserConnecting = document.querySelector('.laser-connecting')
-
-    laserConnecting.style.display = 'block'
-
-    let response = await fetch('/connect', { headers: {}, method: 'POST', body: JSON.stringify({}) })
-
-    let data = await response.json()
-
-    if(data.message){
-      laserConnecting.innerText = data.message
-    } else {
-      laserConnecting.innerText = 'Laser Connected'
-    }
-  })
+})
